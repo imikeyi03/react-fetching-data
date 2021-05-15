b193ba18f079ae686ee08888265f2b34e12a708f
Mikey Mann <mikey.mannx3@gmail.com>
2021-05-15 09:57:07 -0700

    finished giphy axios search api app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
@@ -7,19 +8,56 @@ export default class App extends Component {
   
   constructor() {
     super();
+    this.state = {
+      gifs: [],
+      loading: true
+    }
   } 
 
+
+componentDidMount() {
+ this.performSearch();
+}
+
+performSearch = (query = "awesome") => {
+  // Search Giphy API Endpoint
+  axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+  .then(response => {
+    this.setState({
+      gifs: response.data.data,
+      loading: false 
+    })
+  })
+  .catch(error => {
+    console.log('Error fetching and parsing data', error);
+  });
+}
+
+
+
   render() { 
+    console.log(this.state.gifs)
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch}/>      
           </div>   
-        </div>    
+        </div>  
         <div className="main-content">
-          <GifList />
+        <p>A giphy API search by <b>Mikey Mann</b></p>
+        {
+          // Ternary if else clause
+
+          // If state 'loading' is true,
+          (this.state.loading)
+          // Display Loading
+            ?<p>Loading...</p>
+            // Else, load the GifList component with the data fetched in axios
+            :<GifList data={this.state.gifs} />
+        }
+          
         </div>
       </div>
     );
